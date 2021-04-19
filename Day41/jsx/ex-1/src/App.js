dfb85293258f958e3476f1cac0886f5d26f6ded6
ORG <47654977+ryeongee@users.noreply.github.com>
2021-04-20 00:55:38 +0900

    Day41
    .

---
+++ Day41/jsx/ex-1/src/App.js
@@ -0,0 +1,39 @@
+/* jsx
+function App() {
+  const answer = "리액트";
+  const name = undefined;
+  return name || '값이 undefined 입니다.';
+}
+
+export default App;*/
+
+// function App() {
+//   const answer = "리액트";
+//   const name = undefined;
+//   return name || '값이 undefined 입니다.';
+// }
+
+// export default App;
+
+//class
+// import React, {Component} from 'react';
+// import MyComponent from './MyComponent';
+// class App extends Component {
+//   render(){
+//     const answer = "리액트";
+//     const name = undefined;
+//     return <MyComponent name='React'>리액트</MyComponent>;
+//   }
+// }
+
+// export default App;
+
+import Counter from './Counter';
+
+const App = () => {
+  return (
+    <Counter />
+  );
+};
+
+export default App;
