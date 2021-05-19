bee3761b66545e4023485282a3bc423b11d32f1b
ORG <47654977+ryeongee@users.noreply.github.com>
2021-05-19 14:47:35 +0900

    Update index.js

--- Fastcampus-class/Webpack/module/index.js
+++ Fastcampus-class/Webpack/module/index.js
@@ -1,7 +1,6 @@
 import mathUtil from './mathUtil'
 //const { getCircleArea } = require('./mathUtil');
 
-
 //const result = getCircleArea(2);
 const result = mathUtil.getCircleArea(2);
 console.log(result);
