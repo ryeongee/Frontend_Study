e4a73fe7a252518f721a6b64f38bc4aed365690c
ORG <47654977+ryeongee@users.noreply.github.com>
2021-05-24 15:32:38 +0900

    .
    .

--- Fastcampus-class/Homework/Fin/1/src/exam/login/main.js
+++ Fastcampus-class/Homework/Fin/1/src/exam/login/main.js
@@ -2,6 +2,7 @@
 const login_id = document.getElementById('input__id');
 const login_pw = document.getElementById('input__pw');
 const class_login = document.getElementById('log_btn');
+const warning_word = document.getElementById('warning');
 
 login_pw.addEventListener('keyup', function(){
     var check = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,20}$/.test(login_pw.value);
@@ -9,11 +10,13 @@ login_pw.addEventListener('keyup', function(){
         class_login.style.color = "black";        
         class_login.style.backgroundColor = "greenyellow";
         class_login.style.cursor = "pointer";
+        warning_word.style.textIndent = "-9999px";
     }
     else {
         class_login.style.color = "rgb(82, 81, 81)";
         class_login.style.backgroundColor = "gray";
         class_login.style.cursor = "default";
+        warning_word.style.textIndent = "0";
     }
 });
 
